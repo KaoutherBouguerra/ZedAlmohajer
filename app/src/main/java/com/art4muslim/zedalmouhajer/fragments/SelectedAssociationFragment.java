6c0425008e52776ad2e9913ff3b19332025b1ba8
KaoutherBouguerra <kaouther.bouguerra@gmail.com>
2018-09-26 07:41:30 +0100

    test 2

--- app/src/main/java/com/art4muslim/zedalmouhajer/fragments/SelectedAssociationFragment.java
+++ app/src/main/java/com/art4muslim/zedalmouhajer/fragments/SelectedAssociationFragment.java
@@ -12,31 +12,55 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
 import com.art4muslim.zedalmouhajer.BaseApplication;
 import com.art4muslim.zedalmouhajer.R;
 import com.art4muslim.zedalmouhajer.features.LoginActivity;
 import com.art4muslim.zedalmouhajer.fragments.specific_association.AssociationBeneficiaryFragment;
 import com.art4muslim.zedalmouhajer.fragments.specific_association.NewsBeneficAssociationFragment;
 import com.art4muslim.zedalmouhajer.models.Association;
+import com.art4muslim.zedalmouhajer.session.Constants;
+import com.art4muslim.zedalmouhajer.utils.AlertDialogManager;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.art4muslim.zedalmouhajer.session.SessionManager.KEY_NAME;
+import static com.art4muslim.zedalmouhajer.session.SessionManager.Key_UserID;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SelectedAssociationFragment extends Fragment {
 
-
+    private static final String TAG = SelectedAssociationFragment.class.getSimpleName();
     View v;
 
     LinearLayout linearAbout, linearBen, linearContact, linearRegister;
     ImageView imgAss;
     TextView txtNameAss;
     TextView txt_register;
-
+    ProgressBar _progressBar;
+    LinearLayout _relative;
     boolean isAdded = false;
     Association association;
     @Override
@@ -108,7 +132,7 @@ public class SelectedAssociationFragment extends Fragment {
               //  RegisterToAssociationFragment associationBeneficiaryFragment = new RegisterToAssociationFragment();
               // showFragment(associationBeneficiaryFragment);
 
-
+                attemptRegister();
 
             }
         });
@@ -128,6 +152,8 @@ public class SelectedAssociationFragment extends Fragment {
         imgAss = v.findViewById(R.id.ass_profile_photo);
         txtNameAss = v.findViewById(R.id.txtNameAss);
         txt_register = v.findViewById(R.id.txt_register);
+        _progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        _relative = (LinearLayout) v.findViewById(R.id.relative);
 
 
     }
@@ -141,5 +167,112 @@ public class SelectedAssociationFragment extends Fragment {
         fragmentTransaction.replace(R.id.container, fragment, "home Fragment");
 
         fragmentTransaction.commit();
+    }
+
+
+
+    private void attemptRegister() {
+
+
+        register(BaseApplication.session.getUserDetails().get(Key_UserID), association.getId());
+
+    }
+
+    private void register(final String idBen, final String idAss ) {
+
+        String url = Constants.REGISTER_BENEFIC_TO_ASS;
+        //+"phone="+phone+"&password="+password;
+
+        Log.e(TAG, "register url "+url);
+        Log.e(TAG, "idBen  "+idBen);
+        Log.e(TAG, "idAss "+idAss);
+
+
+        StringRequest LoginFirstRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.v("registerRequest","response == " +response);
+
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(response);
+                    boolean _status = jsonObject.getBoolean("status");
+
+                    if (_status) {
+
+                        SelectedAssociationFragment selectedAssociationFragment = new SelectedAssociationFragment();
+                        Bundle args = new Bundle();
+                        args.putSerializable("ASSOCIATION", association);
+                        selectedAssociationFragment.setArguments(args);
+                        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.container, selectedAssociationFragment, "home Fragment");
+
+                        fragmentTransaction.commit();
+
+
+                    } else {
+
+                        String msg = jsonObject.getString("msg");
+
+                        AlertDialogManager.showAlertDialog(getActivity(),getResources().getString(R.string.app_name),msg,false,0);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                _progressBar.setVisibility(View.GONE);
+                _relative.setVisibility(View.VISIBLE);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                _progressBar.setVisibility(View.GONE);
+                _relative.setVisibility(View.VISIBLE);
+
+                if (error instanceof AuthFailureError) {
+
+                    AlertDialogManager.showAlertDialog(getActivity(),getResources().getString(R.string.app_name),getResources().getString(R.string.authontiation),false,3);
+
+                } else if (error instanceof ServerError) {
+                    AlertDialogManager.showAlertDialog(getActivity(),getResources().getString(R.string.app_name),getResources().getString(R.string.servererror),false,3);
+                } else if (error instanceof NetworkError) {
+                    AlertDialogManager.showAlertDialog(getActivity(),getResources().getString(R.string.networkerror),getResources().getString(R.string.networkerror),false,3);
+
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    AlertDialogManager.showAlertDialog(getActivity(),getResources().getString(R.string.app_name),getResources().getString(R.string.timeouterror),false,3);
+                }
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("bene_id", idBen);
+                params.put("ass_id", idAss);
+
+                return params;
+            }
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+
+                    String json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+                    return Response.success(json, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    return Response.error(new ParseError(e));
+                }
+            }
+        };
+
+
+        // Adding request to request queue
+        BaseApplication.getInstance().addToRequestQueue(LoginFirstRequest);
+
+
     }
 }
